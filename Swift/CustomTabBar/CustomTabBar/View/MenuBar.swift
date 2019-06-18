b493cf8e08268124336f6dabf4101d37203c2937
Wi <dnltjdghks1@gmail.com>
2019-06-18 14:30:04 +0900

    Menubar not complete

---
+++ Swift/CustomTabBar/CustomTabBar/View/MenuBar.swift
@@ -0,0 +1,113 @@
+//
+//  MenuBar.swift
+//  CustomTabBar
+//
+//  Created by Wi on 18/06/2019.
+//  Copyright © 2019 Wi. All rights reserved.
+//
+
+import UIKit
+
+class MenuBar: UIView {
+    
+    let menuCollectionView: UICollectionView = {
+        
+        // flow layout
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        
+        // collection view
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        
+        // not show scroll indicator
+        collectionView.showsHorizontalScrollIndicator = false
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+    
+    let indicatorBar: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    var menuTitles = ["menu1", "menu2", "menu3", "menu4"]
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureCollectionView()
+        autolayout()
+    }
+    
+    private func configureCollectionView(){
+        menuCollectionView.dataSource = self
+        menuCollectionView.delegate = self
+        
+        menuCollectionView.register(MenuCell.self, forCellWithReuseIdentifier: "menuCell")
+    }
+    
+    private func autolayout(){
+        
+        addSubview(menuCollectionView)
+        addSubview(indicatorBar)
+        
+        menuCollectionView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        menuCollectionView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        menuCollectionView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        menuCollectionView.bottomAnchor.constraint(equalTo: indicatorBar.topAnchor).isActive = true
+        
+        // 임시
+        
+        indicatorBar.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        
+        indicatorBar.heightAnchor.constraint(equalToConstant: 2).isActive = true
+        indicatorBar.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
+extension MenuBar: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "menuCell", for: indexPath) as! MenuCell
+        
+        return cell
+    }
+    
+
+}
+
+extension MenuBar: UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let cell = collectionView.cellForItem(at: indexPath) as? MenuCell else {return}
+        
+    }
+    
+}
+
+extension MenuBar: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.frame.width / CGFloat(menuTitles.count), height: 50)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
