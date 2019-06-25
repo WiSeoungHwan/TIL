9444eedb761ecfeace744a61986306d01c108884
WiSeoungHwan <dnltjdghks1@gmail.com>
2019-06-25 21:50:56 +0900

    solution4 not working..

--- Algorithms/AlgorithmsMemo/AlgorithmsMemo/main.swift
+++ Algorithms/AlgorithmsMemo/AlgorithmsMemo/main.swift
@@ -8,23 +8,34 @@
 
 import Foundation
 
-func solution3(_ n:Int, _ lost:[Int], _ reserve: [Int]) -> Int {
-    var notHaveGimSuitStu = [Int]()
+func solution4(_ n:Int, _ lost:[Int], _ reserve: [Int]) -> Int {
+    var haveGimSuitStu = [Int]()
     for i in 1...n{
-        if !(lost.contains(i) && reserve.contains(i)){
-            if lost.contains(i) && !reserve.contains(i + 1) {
-                if !reserve.contains(i - 1){
-                    notHaveGimSuitStu.append(i)
-                    print(notHaveGimSuitStu)
-                }
-                print(notHaveGimSuitStu, i)
-            }
+        if lost.contains(i) && reserve.contains(i){
+            haveGimSuitStu.append(i)
+            continue
         }
+        
+        if !lost.contains(i){
+            haveGimSuitStu.append(i)
+        }else if reserve.contains(i){
+            haveGimSuitStu.append(i)
+        }else if !haveGimSuitStu.contains(i) && reserve.contains(i + 1){
+            haveGimSuitStu.append(i)
+        }else if reserve.contains(i - 1){
+            haveGimSuitStu.append(i)
+        }
+        else {
+            print("else: \(i)")
+        }
+        print(haveGimSuitStu)
     }
-    return n - notHaveGimSuitStu.count
+    return haveGimSuitStu.count
 }
 
 
-//print(solution2(30,[2,3,25,7,8,11,12,18],[1,24,3,5]))
+print(solution4(30,[2,3,25,7,8,11,12,18],[1,24,3,5]))
 
-print(solution3(5, [2,4], [3]))
+print(solution4(5, [2,4],[3]))
+//
+//print(solution3(8, [2,3,7,8], [1,3,5]))
